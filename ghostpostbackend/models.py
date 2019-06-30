b604879ab0b22d003dca389602a71ced6920807b
Michelle Hoelzer <hoelzermm@gmail.com>
2019-06-30 16:23:05 -0400

    models updated to best style?

--- ghostpostbackend/models.py
+++ ghostpostbackend/models.py
@@ -3,18 +3,21 @@ from django.utils import timezone
 
 
 class BoastRoastModel(models.Model):
-    # could do a pick between oast and roast 
-    # one post for baost rast 
-    boast = models.CharField(max_length=280, blank=True)
-    roast = models.CharField(max_length=280, blank=True)
+    # boast = models.CharField(max_length=280, blank=True)
+    # roast = models.CharField(max_length=280, blank=True)
+    # could do a pick between oast and roast
+    # one post for baost rastL https://stackoverflow.com/questions/4320679/django-display-choice-value
+    B = "B"
+    R = "R"
+    BOAST_OR_ROAST = ((B, "Boast"), (R, "Roast"))
+    choice = models.CharField(max_length=280, choices=BOAST_OR_ROAST, default=B)
+    post = models.CharField(max_length=280, default="Write a boast or roast")
     # date = models.DateTimeField(default=timezone.now)
+    # dont need many to many b/c not indepth
     # do 2 of each or just vote count field
     # capture ip adress of whoever votes to check against stuff
     upvotes = models.PositiveIntegerField(default=0, blank=True)
     downvotes = models.PositiveIntegerField(default=0, blank=True)
-    # dont need many to many b/c not indepth
-    # upvotes = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="up")
-    # downvotes = models.ManyToManyField("self", symmetrical=False, blank=True, related_name="down")
 
-    # def __str__(self):
-    #     return self.date
+    def __str__(self):
+        return f"{self.choice}: {self.post}"
